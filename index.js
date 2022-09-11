9544e6b324467a2fbc1dbff7a87bbb952ddbb4e3
Nicolas Rocha Lacerda <94977120+NRLacerda@users.noreply.github.com>
2022-09-11 02:13:10 -0300

    att

--- index.js
+++ index.js
@@ -2,28 +2,31 @@ const express = require('express');
 const app = express()
 const handlebars = require('express-handlebars')
 
-
 //Config
     //Template engine
-app.engine('handlebars', handlebars.engine({defaultLayout:'main'})) // puxa o html  "main"
-app.set('view engine','handlebars')
+
+app.set("view engine", "handlebars");
+app.engine('handlebars', handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts', // puxa o html  "main"
+}));
 
     //Conexão com o BD
 const Sequelize = require('sequelize')
+
 const sequelize = new Sequelize('sistemadecadastro', 'root', '123456', { // nome banco, user, senha, local do host, tipo de banco
         host:"localhost",
         dialect:'mysql'
 })
     // Rotas
     app.get('/', function(req,res){
-        res.render('index')
+        res.render('main', {layout : 'index'});
     })
     app.get('/caduser', function(req, res){
-        res.render('caduser')
+        res.render('caduser', {layout:'caduser'})
     })
 
 
-app.listen(8042, function(){console.log("server running!");});
+app.listen(8045, function(){console.log("server running!");});
 
 
 // futuramente usar nestjs que usa TS
