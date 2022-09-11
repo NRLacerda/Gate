40c9a79999d06ccecad94b703a4669795f39e3f6
Nicolas Rocha Lacerda <94977120+NRLacerda@users.noreply.github.com>
2022-09-11 01:47:06 -0300

    att

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ const handlebars = require('express-handlebars')
     //Template engine
 app.engine('handlebars', handlebars.engine({defaultLayout:'main'})) // puxa o html  "main"
 app.set('view engine','handlebars')
+
     //Conexão com o BD
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('sistemadecadastro', 'root', '123456', { // nome banco, user, senha, local do host, tipo de banco
         host:"localhost",
         dialect:'mysql'
 })
+    // Rotas
+    app.get('/', function(req,res){
+        res.render('index')
+    })
+    app.get('/caduser', function(req, res){
+        res.render('caduser')
+    })
 
 
 app.listen(8042, function(){console.log("server running!");});
