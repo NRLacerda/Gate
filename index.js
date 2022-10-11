64a3864edadf40b937c42f1eb2b5d5fcfa4de1cc
NRLacerda <nrochalacerda@gmail.com>
2022-10-11 10:34:57 -0300

    attt

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function hash() {
 	var crypto = require("crypto");
 	var name = "@n1Mseguranza";
 	var hash = crypto.createHash("md5").update(name).digest("hex");
-	console.log(hash); // 9b74c9897bac770ffc029102a200c5de
+	console.log("Senha random hasheada"+ hash); // 
 }
 //Config
 // Handlebars, carrega os "html"
